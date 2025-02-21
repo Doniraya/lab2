d61d47aaa3fc2a8c2ca75e81b16bec1cd0d56306
student <student@2405-01>
2025-02-21 08:38:02 +0800

    Добавьте файлы проекта.

---
+++ Alarm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Alarm
+    {
+        public uint hours;
+        public uint minutes;
+        public uint alarmTimeHours;
+        public uint alarmTimeMinutes;
+
+        public Alarm(uint hours, uint minutes, uint alarmTimeHours, uint alarmTimeMinutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.alarmTimeHours = alarmTimeHours;
+            this.alarmTimeMinutes = alarmTimeMinutes;
+
+        }
+
+        public Alarm(string time0, string time)
+        {
+            string[] s = time.Split(':');
+            alarmTimeHours = Convert.ToUInt32(s[0]);
+            alarmTimeMinutes = Convert.ToUInt32(s[1]);
+            string[] s0 = time0.Split(':');
+            hours = Convert.ToUInt32(s0[0]);
+            minutes = Convert.ToUInt32(s0[1]);
+         
+
+        }
+        private void Ring()
+        {
+            Console.WriteLine("ring");
+        }
+
+        public uint CheckTimeLeft()
+        {
+            uint time1 = hours * 60 + minutes;
+            uint time2 = alarmTimeHours * 60 + alarmTimeMinutes;
+
+            if (time2 - time1 <= 0)
+            {
+                Ring();
+                return 0;             
+            }
+            else
+            {
+                return time2 - time1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using lab2;
+
+Alarm alarm = new Alarm(12, 30, 12, 30);
+Alarm alarm1 = new Alarm("9:10", "11:30");
+
+Console.WriteLine($"{alarm.CheckTimeLeft()} minutes left");
+
+Console.WriteLine($"{alarm1.CheckTimeLeft()} minutes left");
+
